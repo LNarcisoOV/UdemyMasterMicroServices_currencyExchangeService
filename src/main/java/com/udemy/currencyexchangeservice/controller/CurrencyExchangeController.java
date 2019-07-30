010f1a8cd42373d9ffb3a83111f977109b073ca4
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-30 14:25:49 -0300

    Implementing integrated logging.

--- src/main/java/com/udemy/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/udemy/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.udemy.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import com.udemy.currencyexchangeservice.repository.ExchangeValueRepository;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private Environment environment;
 
@@ -23,13 +27,17 @@ public class CurrencyExchangeController {
 	public ExchangeValue retrieveExchangeValue(@PathVariable String fromCurrency,
 			@PathVariable String toCurrency) {
 
-		ExchangeValue exchangeValue = exchangeValueRepository.findByFromCurrencyAndToCurrency(fromCurrency, toCurrency);
+		ExchangeValue exchangeValue = exchangeValueRepository.findByFromCurrencyAndToCurrency(fromCurrency,
+				toCurrency);
 
 		if (exchangeValue == null) {
 			throw new ExchangeValueNotFoundException("Exchange value not found");
 		}
 
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		
+		logger.info("{}", exchangeValue);
+		
 		return exchangeValue;
 	}
 }
