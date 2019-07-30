ba6da3c88da09317bfc8d64fab4ff6a1454e887c
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-30 11:55:43 -0300

    Implmenting spring cloud sleuth.

--- src/main/java/com/udemy/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/udemy/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,6 +3,9 @@ package com.udemy.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaulSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
